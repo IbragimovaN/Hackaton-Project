4e536af7fb9de3798738833f1701bce2156383f8
Anastasiya <anastasiya.ibr@mail.ru>
2023-11-04 21:19:57 +0300

    master add HTML render

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -8,6 +8,23 @@ export class BackgroundModule extends Module {
     trigger() {  
         document.body.style.background = getRandomColor()
     }
+    render(){
+        const header = document.createElement('div')
+        header.classList = 'header'
+
+        const titleMini = document.createElement('h3')
+        titleMini.classList = 'header-subtitle'
+        titleMini.textContent = 'Демо-сайт'
+
+        const titleBig = document.createElement('h1')
+        titleBig.classList = 'header-title'
+        titleBig.textContent = 'Чему можно научиться на первом модуле курса Джуниор Frontend-разработчик'
+
+        
+
+    
+
+    }
     
 
 }
