6a6aa489153ca45cf65138916db783c658c234c3
Mark Seweke <750581@list.ru>
2023-11-05 21:39:35 +0400

    develop add btn

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,14 @@ export function getRandomColor() {
   }
   return color;
 }
+
+export function addButtonAndClose (container){
+  const closeButton = document.createElement("div");
+  closeButton.className = "cl-btn-7";
+  container.append(closeButton)
+
+  closeButton.addEventListener("click", () => {
+      container.classList.add('hidden')
+      location.reload();
+   });
+}
